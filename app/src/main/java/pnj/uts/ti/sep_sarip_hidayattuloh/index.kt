9e4d30b10cb9829b29a10f70e0adc6aedf328a53
sepsarip <sepsarip1811@gmail.com>
2024-05-19 17:12:43 +0700

    option menu, tambahdata, data alumni, logout

--- app/src/main/java/pnj/uts/ti/sep_sarip_hidayattuloh/index.kt
+++ app/src/main/java/pnj/uts/ti/sep_sarip_hidayattuloh/index.kt
@@ -1,12 +1,17 @@
 package pnj.uts.ti.sep_sarip_hidayattuloh
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.fragment.app.Fragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlin.math.log
 
 class index : AppCompatActivity() {
 
@@ -21,6 +26,9 @@ class index : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
 
         bottomNav = findViewById(R.id.bottomNav) as BottomNavigationView
         bottomNav.setOnItemSelectedListener { item ->
@@ -41,7 +49,42 @@ class index : AppCompatActivity() {
             }
         }
         loadFragment(HomeFragment())
+
+
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.optionmenu,menu)
+        return true
+    }
+
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.tambahdata -> {
+                val intent = Intent(this, tambahdata::class.java)
+                startActivity(intent)
+                finish()
+                return true
+            }
+
+            R.id.dataalumni -> {
+                val intent = Intent(this, dataalumni::class.java)
+                startActivity(intent)
+                finish()
+                return true
+            }
+
+            R.id.logout -> {
+                val intent = Intent(this, loginform::class.java)
+                startActivity(intent)
+                finish()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
 
 
     private fun loadFragment(fragment: Fragment) {
