fbb51de864bdbc22e7cb5e0b51ae4e20964924c9
sepsarip <sepsarip1811@gmail.com>
2024-05-19 16:42:55 +0700

    detail berita,add layout alumni

---
+++ app/src/main/java/pnj/uts/ti/sep_sarip_hidayattuloh/DetailDataAlumni.kt
@@ -0,0 +1,20 @@
+package pnj.uts.ti.sep_sarip_hidayattuloh
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class DetailDataAlumni : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_detail_data_alumni)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
